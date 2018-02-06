ab1407dc9a4bcc60481e79d8932efacd6adba8fe
Aurelien Callens <aurelien@seen-apps.com>
2018-02-06 16:40:09 +0100

    Updated InputField and TextAreField to work with new properties. Added CSS validation error

--- src/client/app/components/InputField.jsx
+++ src/client/app/components/InputField.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InputField = ({ title, name, type }) => (
+const InputField = ({ title, name, type, field, onChange }) => (
   <div className="field">
     <label htmlFor={name}>{title}</label>
-    <input type={type} name={name} id={name} />
+    <input className={field.hasErrors ? 'has-errors' : ''} type={type} name={name} id={name} value={field.value} onChange={onChange} />
   </div>
 );
 
@@ -12,10 +12,13 @@ InputField.propTypes = {
   title: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   type: PropTypes.string,
+  field: PropTypes.object.isRequired,
+  onChange: PropTypes.func,
 };
 
 InputField.defaultProps = {
   type: 'text',
+  onChange: undefined,
 };
 
 export default InputField;
--- src/client/app/components/TextAreaField.jsx
+++ src/client/app/components/TextAreaField.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TextAreaField = ({ title, name }) => (
+const TextAreaField = ({ title, name, field, onChange }) => (
   <div className="field">
     <label htmlFor={name}>{title}</label>
-    <textarea name={name} id={name} rows="4"></textarea>
+    <textarea className={field.hasErrors ? 'has-errors' : ''} name={name} id={name} rows="4" onChange={onChange} value={field.value} />
   </div>
 );
 
 TextAreaField.propTypes = {
   title: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  field: PropTypes.object.isRequired,
+  onChange: PropTypes.func,
+};
+
+TextAreaField.defaultProps = {
+  onChange: undefined,
 };
 
 export default TextAreaField;
